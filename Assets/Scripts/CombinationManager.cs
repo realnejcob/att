fb099a9b43bf8824720dded52a76932108cc5354
nejcob <jacobhvidamstrup@gmail.com>
2023-05-02 13:11:31 +0200

    Basic implementation of CombinationManager (numpad) and Yarn merge.

--- Assets/Scripts/CombinationManager.cs
+++ Assets/Scripts/CombinationManager.cs
@@ -1,12 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Yarn.Unity;
 
 public class CombinationManager : MonoBehaviour {
 
     public static CombinationManager Instance;
 
     [SerializeField] [ReadOnly] private string currentCombination;
+    [SerializeField] private DialogueRunner dialogueRunner;
+    [SerializeField] private OptionsListViewCustom optionsListView;
 
     private void Awake() {
         if (Instance == null) {
@@ -26,7 +30,20 @@ public class CombinationManager : MonoBehaviour {
     }
 
     public void CommitCombination() {
+        if (!optionsListView.IsWaitingForOption) {
+            Debug.LogWarning("Not ready for option!");
+            return;
+        }
 
+        var options = optionsListView.Options;
+        foreach (var option in options) {
+            if (option.Line.Metadata[0] == currentCombination) {
+                optionsListView.OptionViewWasSelected(option);
+                return;
+            }
+        }
+
+        Debug.LogWarning("No match!");
     }
 
     public void ClearCombination() {
--- Assets/Scripts/CustomYarn/OptionsListViewCustom.cs
+++ Assets/Scripts/CustomYarn/OptionsListViewCustom.cs
@@ -6,13 +6,34 @@ using UnityEngine.UIElements;
 using Yarn.Unity;
 
 public class OptionsListViewCustom : OptionsListView {
+    public DialogueOption[] Options { get; private set; }
+    private Action<int> _onOptionSelected;
+    public bool IsWaitingForOption { get; private set; } = false;
+
     public override void RunOptions(DialogueOption[] dialogueOptions, Action<int> onOptionSelected) {
         base.RunOptions(dialogueOptions, onOptionSelected);
 
+        _onOptionSelected = onOptionSelected;
+        Options = dialogueOptions;
+        IsWaitingForOption = true;
+
         foreach (var option in dialogueOptions) {
             if (option.Line.Metadata != null) {
-                print($"Has key: { option.Line.Metadata[0]}");
+                print($"Option '{option.Line.Text.Text}' has key '{option.Line.Metadata[0]}'");
+            } else {
+                Debug.LogWarning($"Option '{option.Line.Text.Text}' is missing a key!");
             }
         }
+    }
+
+    public void OptionViewWasSelected(DialogueOption option) {
+        StartCoroutine(OptionViewWasSelectedInternal(option));
+    }
+
+    IEnumerator OptionViewWasSelectedInternal(DialogueOption selectedOption) {
+        yield return StartCoroutine(Effects.FadeAlpha(GetComponent<CanvasGroup>(), 1f, 0f, 0.1f));
+        _onOptionSelected(selectedOption.DialogueOptionID);
+        Options = new DialogueOption[] { };
+        IsWaitingForOption = false;
     }
 }
